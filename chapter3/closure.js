dc68e17299abf94c83b586a19d3f4c30ff2d7cd8
Jacques Begin <jacques.andre.begin@gmail.com>
2018-07-11 02:40:07 -0400

    Creates closure file.

---
+++ chapter3/closure.js
@@ -0,0 +1,22 @@
+
+// Simple closure that provides access to the local binding 
+// of a function. In this example it is demonstrated that 
+// parameters passed to functions are automatically locally 
+// binded through the parameter itself. (Setting a parameter
+// to another local binding is unnecessary to bind it to that
+// function).
+
+function closure(n) {
+  let obj = {
+    one: 1 * n,
+    two: 2 * n,
+    three: function() {
+      return n * n;
+    }
+  }
+  return () => obj.three;
+}
+
+let closure1 = closure(2);
+
+console.log(closure1()());
