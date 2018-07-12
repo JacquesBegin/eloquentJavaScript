c87b26c8d320cb8b5982765fd4bde654aefa17d8
Jacques Begin <jacques.andre.begin@gmail.com>
2018-07-12 00:58:10 -0400

    Create a function to avoid repeating code.

--- chapter3/growingFunctions.js
+++ chapter3/growingFunctions.js
@@ -1,16 +1,31 @@
 countAnimals = (numCows, numChickens) => {
-  console.log(countAnimal(numCows) + " Cows");
-  console.log(countAnimal(numChickens) + " Chickens");
+  console.log(zeroPad(numCows) + " Cows");
+  console.log(zeroPad(numChickens) + " Chickens");
 }
 
-countAnimal = (count) => {
-  if (count < 10) {
-    return "00" + count;
-  } else if (count < 100) {
-    return "0" + count;
-  } else {
-    return count;
+zeroPad = (count) => {
+  let str = String(count);
+  while (str.length < 3) {
+    str = "0" + str;
   }
+  return str;
+
+  // let str = (typeof count === "string") ? count : String(count);
+  // console.log(str);
+  // if (str.length < 3) {
+  //   countAnimal(str = "0" + str);
+  // } else {
+  //   return str;
+  // }
+  // return str;
+
+  // if (count < 10) {
+  //   return "00" + count;
+  // } else if (count < 100) {
+  //   return "0" + count;
+  // } else {
+  //   return count;
+  // }
 }
 
 countAnimals(3, 17);
