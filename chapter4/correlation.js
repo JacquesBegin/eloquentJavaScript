9c43deb90c90a0c8b2b33205e541f36e2f9974a0
Jacques Begin <jacques.andre.begin@gmail.com>
2018-07-12 20:04:15 -0400

    Creates a function to find the phi coefficient from the results of a correlation table.

---
+++ chapter4/correlation.js
@@ -0,0 +1,17 @@
+// Compute the phi cofficient
+
+
+// This will produce a number between -1 and 1
+// Numbers closer to 0 will have no/less correlation,
+// numbers closer to 1 will have more correlation
+// for being found together, numbers closer to -1 will
+// have more correlation for not being found together.
+phiCoff = ([n00, n01, n10, n11]) => {
+  return ((n11 * n00) - (n10 * n01)) /
+          Math.sqrt((n10 + n11) * (n00 + n01) * 
+          (n01 + n11) * (n00 + n10))
+}
+
+let arr = [76, 9, 4, 1];
+
+console.log(phiCoff(arr));
