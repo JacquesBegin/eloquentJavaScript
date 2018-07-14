f3ca32ff16d8d3d48846801e3ec429ade01d7c8e
Jacques Begin <jacques.andre.begin@gmail.com>
2018-07-14 00:24:18 -0400

    Finishes reverseArrayInPlace function.

--- chapter4/reverseArray.js
+++ chapter4/reverseArray.js
@@ -1,4 +1,4 @@
-let arr = ["dog", "cat", "bird"];
+let arr = ["dog", "cat", "bird", "pig", "cow", "chicken", "rat"];
 
 function reverseArray(arr) {
   let newArr = [];
@@ -6,14 +6,19 @@ function reverseArray(arr) {
   return newArr;
 }
 
+
+// Reverse the order of an array without creating a new array
 reverseArrayInPlace = (arr) => {
   let temp;
-  for (let i of arr) {
-
+  for (let i = 0; i < Math.floor(arr.length / 2); i++) {
+    temp = arr[i];
+    arr[i] = arr[arr.length - (i + 1)];
+    arr[arr.length - (i + 1)] = temp;
   }
+  return arr;
 }
 
 
 console.log(reverseArray(arr));
-console.log(arr);
+console.log(reverseArrayInPlace(arr));
 // console.log(newArr);
