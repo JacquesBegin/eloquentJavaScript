0cc75027e0d45b42091705b0fa32cc45b7772510
Jacques Begin <jacques.andre.begin@gmail.com>
2018-07-13 21:08:24 -0400

    Testing string methods.

---
+++ chapter4/stringology.js
@@ -0,0 +1,12 @@
+let str = "welcome";
+
+console.log(str.length);
+console.log(str[0]);
+console.log(str.split("c"));
+console.log(["car", "ot"].join("r"));
+console.log(str.padStart(12, "0"));
+console.log("  \n hello ".trim());
+console.log(str.slice(2, 6));
+console.log(str.indexOf("l"));
+console.log(str.indexOf("co"));
+console.log(str.repeat(3));
