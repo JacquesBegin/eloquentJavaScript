b383e14e79bf2022eee705dda17cd8cd6bcf38df
Jacques Begin <jacques.andre.begin@gmail.com>
2018-07-16 19:49:49 -0400

    Creates array to list and list to array functionality.

--- chapter4/arrayToList.js
+++ chapter4/arrayToList.js
@@ -11,15 +11,50 @@ let list = {
 
 // Convert an array to a list (Object)
 function arrayToList(arr) {
-  let obj = {};
+  let obj = null;
   let arrLastIndex = arr.length - 1;
   for (arrLastIndex; arrLastIndex >= 0; arrLastIndex--) {
-    console.log(arrLastIndex);
-    // obj = makeObject(obj, arr[arrLastIndex]);
     obj = {value: arr[arrLastIndex], rest: obj}
   }
-
   return obj;
 }
+console.log(arrayToList([1, 2, 3]));
 
-console.log(arrayToList([1, 2, 3, 4]));
+// Convert a list (Object) to an array.
+
+let testObj = {
+  value: 1,
+  rest: {
+    value: 2,
+    rest: {
+      value: 3,
+      rest: {
+        value: 4,
+        rest: {
+          value: 5,
+          rest: null
+        }
+      }
+    }
+  }
+}
+
+function listToArray(testObj) {
+  let tempObj = Object.create(testObj);
+  let arr = [];
+  let restNotNull = true;
+
+  do {
+    if (tempObj !== null) {
+      arr.push(tempObj.value);
+      tempObj = tempObj.rest;
+    } else {
+      restNotNull = false;
+    }
+  } while (restNotNull);
+  return arr;
+}
+
+console.log(listToArray(arrayToList([1, 2, 3])));
+
+
