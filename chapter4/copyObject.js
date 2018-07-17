345eafd72e97c2e81cb9d00a585625072904705c
Jacques Begin <jacques.andre.begin@gmail.com>
2018-07-16 20:25:13 -0400

    Fixes copyObject function to validate object properties properly.

--- chapter4/copyObject.js
+++ chapter4/copyObject.js
@@ -16,20 +16,18 @@ let obj = {
 }
 
 // This creates a second reference to the value of obj.
-// var newObj = obj;
+// let newObj = obj;
 
 // This creates an actual copy of the obj data.
-// var newObj = JSON.parse(JSON.stringify(obj));
+// let newObj = JSON.parse(JSON.stringify(obj));
 
 // This creates an actual copy of the obj data.
 function copyObject(originalObj) {
   var copiedObj = {};
-  for (let key in originalObj) {
-    console.log("originalObj[key]", originalObj[key]);
-    if (originalObj[key] !== null && typeof(originalObj[key] === "object")) {
+  for (var key in originalObj) {
+    if (originalObj[key] !== null && typeof(originalObj[key]) === "object") {
       copiedObj[key] = copyObject(originalObj[key]);
     } else {
-      console.log("originalObj[key]: ", originalObj[key])
       copiedObj[key] = originalObj[key];
     }
   }
@@ -39,7 +37,7 @@ function copyObject(originalObj) {
 let newObj = copyObject(obj);
 
 
-// newObj.value = 6;
+newObj.value = 6;
 
 
 console.log("obj: ", obj);
