b5a6d92204220c5d897a7fcc2087d7f516df208b
Jacques Begin <jacques.andre.begin@gmail.com>
2018-07-19 22:52:13 -0400

    More changes to the deep equals functionality.

--- chapter4/deepComparison.js
+++ chapter4/deepComparison.js
@@ -1,4 +1,4 @@
-function deepEqual(value1, value2) {
+function deepEqualTest(value1, value2) {
   let areEqual;
   if ((typeof(value1) === "object" && value1 !== null) 
   && 
@@ -12,7 +12,7 @@ function deepEqual(value1, value2) {
       // console.log(`el: ${el}, value1[el] ${value1[el]}`)
       if (value2Arr.includes(el)) {
         // console.log(`includes something`)
-        areEqual = deepEqual(value1[el], value2[el]);
+        areEqual = deepEqualTest(value1[el], value2[el]);
       } else {
         areEqual = false;
       }
@@ -33,53 +33,62 @@ function deepEqual(value1, value2) {
   // return areEqual;
 }
 
-function deepEqualTest(obj1, obj2) {
-  let areEqual;
+function deepEqual(obj1, obj2) {
+  let areEqual = true;
   if ((typeof(obj1) === "object" && obj1 !== null)
       &&
       (typeof(obj2) === "object" && obj2 !== null)) {
-        console.log(`both objs should be objects and not null
-  obj1: ${obj1}, typeof obj1: ${typeof(obj1)}
-  obj2: ${obj2}, typeof obj2: ${typeof(obj2)}`);
     let keyArr1 = Object.keys(obj1);
     let keyArr2 = Object.keys(obj2);
-        console.log(`Value of both keyArr's-> keyArr1: ${keyArr1}, keyArr2: ${keyArr2}`);
+    console.log(`Value of both keyArr's-> keyArr1: ${keyArr1}, keyArr2: ${keyArr2}`);
     if (keyArr1.length == keyArr2.length) {
-      console.log("Arrays have same lengths.");
-      // Here start comparing object properties
-      // Object.keys
+      console.log(`Arrays have equal lengths`);
+    //   for (let el in obj1) {
+    //     // console.log(`el ${el}`);
+    //     if (obj2[el]) {
+    //       areEqual = deepEqual(obj1[el], obj2[el]);
+    //     } else {
+    //       // console.log("Properties are not equal.")
+    //       return false;
+    //     }
+    //   }
     } else {
       console.log("Arrays have different lengths.");
-      return false;
+    //   return false;
     }
-  } else if ((obj1 === obj2)) {
+  } else if ((obj1 !== obj2)) {
     console.log(`both are:->
   obj1: ${obj1} typeof: ${typeof(obj1)},
   obj2: ${obj2} typeof: ${typeof(obj2)}`);
-    areEqual = true;
+    areEqual = false;
   }
-  // Use the following return to default to false.
-  // return (areEqual || false);
+
+  // if (areEqual === true) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
   return (areEqual);
 }
 
 let obj = {here: {is: "an"}, object: 2};
-// console.log(deepEqual(obj, obj));
-// // → true
-// console.log(deepEqual(obj, {here: 1, object: 2}));
-// // → false
-// console.log(deepEqual({here: 1, rest: {value: 1}, object: 2}, {here: 1, rest: {value: 1}, object: 2}));
-// // -> false
-// console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
-// // → true
-// console.log(deepEqual("hi", "hi"));
-// // -> true
+console.log(deepEqual(obj, obj));
+// → true
+console.log(deepEqual(obj, {here: 1, object: 2}));
+// → false
+console.log(deepEqual({here: 1, rest: {value: 1}, object: 2}, {here: 1, object: 2}));
+// -> false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// → true
+console.log(deepEqual("hi", "hi"));
+// -> true
 
 
 
-console.log(deepEqualTest(null, null));
-console.log(deepEqualTest(false, false));
-console.log(deepEqualTest("hi", "hi"));
-console.log(deepEqualTest(1, 1));
-console.log(deepEqualTest(obj, obj));
-console.log(deepEqualTest(obj, {value: 1, rest: obj, str: "hello"}));
+// console.log(deepEqualTest(null, null));
+// console.log(deepEqualTest(false, false));
+// console.log(deepEqualTest("hi", "hi"));
+// console.log(deepEqualTest(1, 1));
+// console.log(deepEqualTest(obj, obj));
+// console.log(deepEqualTest(obj, {value: 1, rest: obj, str: "hello"}));
+// console.log(deepEqualTest(obj, {value: 1, rest: obj}));
