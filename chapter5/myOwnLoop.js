ba56fe94fb6c78d1b19254233a9602c1b9f2964f
Jacques Begin <jacques.andre.begin@gmail.com>
2018-07-22 13:06:00 -0400

    Creates a loop function that accepts a starting value, test/update/body functions, and repeats running those functions until the test fails.

---
+++ chapter5/myOwnLoop.js
@@ -0,0 +1,9 @@
+function loop(value, test, update, body) {
+  while (test(value)) {
+    body(value);
+    value = update(value);
+  }
+}
+
+loop(0, (n) => n < 10, (n) => n += 1, console.log);
+loop(3, n => n > 0, n => n - 1, console.log);
