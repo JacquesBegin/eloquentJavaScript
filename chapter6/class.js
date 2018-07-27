293e517b1429cd40452a5c83c2f65d8bbbaa7f4a
Jacques Begin <jacques.andre.begin@gmail.com>
2018-07-27 16:45:49 -0400

    Practice creating classes and properties.

---
+++ chapter6/class.js
@@ -0,0 +1,25 @@
+class Rabbit {
+  constructor(type) {
+    this.type = type;
+  }
+  speak(line) {
+    console.log(`The ${this.type} rabbit says '${line}'`);
+  }
+}
+
+let killerRabbit = new Rabbit("killer");
+let blackRabbit = new Rabbit("black");
+
+killerRabbit.speak("Hi there");
+blackRabbit.speak("Hello");
+
+Rabbit.prototype.teeth = "small";
+console.log(killerRabbit.teeth);
+// → small
+killerRabbit.teeth = "long, sharp, and bloody";
+console.log(killerRabbit.teeth);
+// → long, sharp, and bloody
+console.log(blackRabbit.teeth);
+// → small
+console.log(Object.getPrototypeOf(killerRabbit).teeth);
+// → small
