5c3c78b606f7964d1f163f018693e704835663db
Jacques Begin <jacques.andre.begin@gmail.com>
2018-07-27 14:34:20 -0400

    Modifying object prototypes and inheriting.

--- chapter6/prototypes.js
+++ chapter6/prototypes.js
@@ -3,3 +3,24 @@ let empty = {};
 console.log(empty.toString);
 console.log(empty.toString());
 
+console.log(Object.getPrototypeOf({}) == Object.prototype);
+console.log(Object.getPrototypeOf(Object.prototype));
+
+let protoRabbit = {
+  speak(line) {
+    console.log(`The ${this.type} rabbit says '${line}'`);
+  }
+}
+
+let killerRabbit = Object.create(protoRabbit);
+killerRabbit.type = "killer";
+killerRabbit.speak("SKREEEE!");
+
+killerRabbit.speak = function (line) {
+  console.log(`The ${this.type} rabbit says 'I'm a nice bunny.....really....${line}'`);
+}
+
+killerRabbit.speak("AHHHH!");
+let brownRabbit = Object.create(protoRabbit);
+brownRabbit.type = "brown";
+brownRabbit.speak("...nibble...");
