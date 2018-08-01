9db6ad645005bb1b1d88dc58fdd49b9cda8abfcc
Jacques Begin <jacques.andre.begin@gmail.com>
2018-07-31 20:32:18 -0400

    Assembling a list of destinations given an array of strings representing all the possible roads.

--- chapter7/robot.js
+++ chapter7/robot.js
@@ -8,3 +8,34 @@ const roads = [
   "Marketplace-Town Hall",       "Shop-Town Hall"
 ];
 
+let destinations = {}; 
+for (let road of roads) {
+  let segment = road.split("-");
+  if (destinations.hasOwnProperty(segment[0])) {
+    if (!destinations[segment[0]].includes(segment[1])) {
+      destinations[segment[0]].push(segment[1]);
+    }
+  } else {
+    destinations[segment[0]] = [segment[1]];
+  }
+  if (destinations.hasOwnProperty(segment[1])) {
+    if (!destinations[segment[1]].includes(segment[0])) {
+      destinations[segment[1]].push(segment[0]);
+    }
+  } else {
+    destinations[segment[1]] = [segment[0]];
+  }
+}
+
+// console.log(destinations);
+
+function displayDestinations(startingLocation) {
+  let destinationPrintout = `Destinations from ${startingLocation}:`;
+  if (destinations[startingLocation]) {
+    for (let dest of destinations[startingLocation]) {
+      destinationPrintout += `\n${dest}`;
+    }
+  }
+  console.log(destinationPrintout);
+}
+displayDestinations("Alice's House");
